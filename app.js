d21b9df4923bf887880fbeed1bce882c32b20687
Monir <monirhrabby.personal@gmail.com>
2023-03-05 17:13:28 +0600

    server create

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+// middleware
+
+app.use(express.json());
+app.use(cors());
+
+//routes routes
+
+//home
+app.get("/", (req, res) => {
+    res.send("Route is working");
+});
+
+process.on("warning", warning => {
+    console.log(console.stack);
+});
+
+module.exports = app;
